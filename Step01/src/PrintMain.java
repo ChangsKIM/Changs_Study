8bb93d3f09beadd859a0e1ab8a2eec6ed28a0720
nam2626 <nam2626@naver.com>
2024-08-28 15:33:36 +0900

    테스트

--- Step01/src/PrintMain.java
+++ Step01/src/PrintMain.java
@@ -9,8 +9,13 @@ public class PrintMain {
 		System.out.println("Hello");
 		System.out.println("Hello" + "World");
 		System.out.println("Hello"+10);
-		
-		System.out.println("안녕하세요" + 8282 + 1000 + "갑시다." + 1234 + 1000);
+		//안녕하세요82821000갑시다12341000 
+		//TEST
+		System.out.println("안녕하세요" + 8282 + 1000 
+				+ "갑시다." + 1234 + 1000);
+		//안녕하세요9282갑시다2234
+		System.out.println("안녕하세요" + (8282 + 1000) 
+				+ "갑시다." + (1234 + 1000));
 	}
 
 }
