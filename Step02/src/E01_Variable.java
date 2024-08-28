c03fc9fc6340485c7824e06849982fd126a70dc6
nam2626 <nam2626@naver.com>
2024-08-28 16:08:47 +0900

    변수, 데이터 형태

---
+++ Step02/src/E01_Variable.java
@@ -0,0 +1,21 @@
+
+public class E01_Variable {
+
+	public static void main(String[] args) {
+		/*
+		 * 	데이터 형태
+		 * 		정수 : 양수, 음수, 0
+		 * 		실수 : 정수에서 소수점이 추가된 형태
+		 * 		문자 : 한 글자
+		 * 		논리 : 참(true), 거짓(false)
+		 * 		문자열 : 문자가 한개이상 구성된 형태
+		 * 
+		 * 	변수(Variable) : 데이터를 하나 저장할 공간
+		 * 
+		 * 	변수 선언 방법
+		 * 		변수타입 변수명; 
+		 */
+
+	}
+
+}
