9d754decce1624410ff1b700176ebb5a201b2031
nam2626 <nam2626@naver.com>
2024-08-28 17:20:30 +0900

    변수 타입, 선언, 초기화, 저장 범위

---
+++ Step02/src/E02_VariableType.java
@@ -0,0 +1,29 @@
+
+public class E02_VariableType {
+	public static void main(String[] args) {
+		/*
+		 * 	변수 타입
+		 * 		정수형 : byte(1), short(2), int(4), long(8)
+		 * 		실수형 : float(4), double(8)
+		 * 		문자형 : char(2)
+		 * 		논리형 : boolean(1)
+		 */
+		int i = 2147483647;
+		System.out.println(i);
+		
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		
+		short s = 32767;
+		System.out.println(s);
+		
+		byte b = 127;
+		System.out.println(b);
+		
+		b += 1;
+		System.out.println(b);
+	}
+}
+
+
+
