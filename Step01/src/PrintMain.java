93dec7570e4554d4065edeb42c38de1ec3e38170
nam2626 <nam2626@naver.com>
2024-08-28 15:47:16 +0900

    printf, 서식 문자열

--- Step01/src/PrintMain.java
+++ Step01/src/PrintMain.java
@@ -10,11 +10,15 @@ public class PrintMain {
 		System.out.println("Hello" + "World");
 		System.out.println("Hello"+10);
 		//안녕하세요82821000갑시다12341000 
-		System.out.println("안녕하세요" + 8282 + 1000 
-				+ "갑시다." + 1234 + 1000);
+		System.out.println("안녕하세요" + 8282 + 1000 + "갑시다." + 1234 + 1000);
 		//안녕하세요9282갑시다2234
-		System.out.println("안녕하세요" + (8282 + 1000) 
-				+ "갑시다." + (1234 + 1000));
+		System.out.println("안녕하세요" + (8282 + 1000) + "갑시다." + (1234 + 1000));
+		System.out.println("----------------------");
+		System.out.print("Hello\n");
+		System.out.print("World");
+		System.out.println("----------------------");
+		System.out.printf("안녕하세요 %d %d \n",8282,1000);
+		
 	}
 
 }
