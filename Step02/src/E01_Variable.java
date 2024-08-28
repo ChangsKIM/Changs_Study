97ac5e292a9f9ff5a095d382f182438e03991a4c
nam2626 <nam2626@naver.com>
2024-08-28 16:19:06 +0900

    변수 초기화

--- Step02/src/E01_Variable.java
+++ Step02/src/E01_Variable.java
@@ -15,7 +15,27 @@ public class E01_Variable {
 		 * 	변수 선언 방법
 		 * 		변수타입 변수명; 
 		 */
-
+		
+		//변수 선언 방법 - 1
+		int num;//num이라는 이름으로 정수형 데이터를 하나 저장할 공간을 임대 받았다
+		num = 100;//정수 100을 변수 num에 저장하겠다.
+		System.out.println(num);//num이 저장하고 있는 100을 읽어와서 화면에 출력
+		num = 200;
+		System.out.println(num);
+		
+		//변수 선언 방법 - 2
+		//초기화(initialize) : 변수를 선언한 뒤에 처음 저장해는 행위
+		int a = 10;
+		System.out.println(a);
+		
+				
 	}
 
 }
+
+
+
+
+
+
+
