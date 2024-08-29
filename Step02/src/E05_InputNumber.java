23a565b20381cf2d5a9240f525c788872edeb1f8
nam2626 <nam2626@naver.com>
2024-08-29 13:55:20 +0900

    숫자 입력

---
+++ Step02/src/E05_InputNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class E05_InputNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = 0;
+		System.out.print("숫자 입력 > ");
+		n1 = sc.nextInt(); //실제 숫자를 입력 받고 난 뒤에 n1에 저장
+		System.out.println("n1 : " + n1 );
+		
+		System.out.print("숫자 입력 > ");
+		int n2 = sc.nextInt();
+		System.out.println("n2 : " + n2);
+		
+		//실수 하나 입력 받아서 출력
+		System.out.print("실수 입력 > ");
+		double f = sc.nextDouble();
+		System.out.println("f : " + f);
+	}
+}
+
+
+
+
