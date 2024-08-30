deac6c3e7365f6b39af65eb59d136ac1320b05b4
nam2626 <nam2626@naver.com>
2024-08-30 13:55:43 +0900

    else if

---
+++ Step03/src/E06_else_if.java
@@ -0,0 +1,48 @@
+
+public class E06_else_if {
+	/*
+	 * 		else if
+	 * 			if문으로 구성된 조건문에서 두번째 조건문부터 담당하는 문법
+	 * 
+	 * 		if(조건식-1){
+	 * 			실행할 코드...
+	 * 		} else if(조건식-2){
+	 * 			실행할 코드...
+	 * 		} ... 
+	 * 		else{
+	 * 			실행할 코드...
+	 * 		}
+	 * 
+	 */
+	public static void main(String[] args) {
+		//성인 20세 이상, 고등학생 17~19, 중학생 14~16, 초등학생 8~13, 미취학 아동
+		int age = 16;
+		
+		if(age >= 20) {
+			System.out.println("성인");
+		}else if(age >= 17) {
+			System.out.println("고등학생");
+		}else if(age >= 14) {
+			System.out.println("중학생");
+		}else if(age >= 8) {
+			System.out.println("초등학생");
+		}else {
+			System.out.println("미취학 아동");
+		}
+		//------------------
+		if() {
+			System.out.println("미취학 아동");
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
