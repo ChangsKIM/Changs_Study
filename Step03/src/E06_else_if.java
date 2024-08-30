63a42eb07c8aa736f5ccb48167eb8577b4d2c0bb
nam2626 <nam2626@naver.com>
2024-08-30 14:06:13 +0900

    else if

--- Step03/src/E06_else_if.java
+++ Step03/src/E06_else_if.java
@@ -30,8 +30,16 @@ public class E06_else_if {
 			System.out.println("미취학 아동");
 		}
 		//------------------
-		if() {
+		if(age <= 7) {
 			System.out.println("미취학 아동");
+		}else if(age <= 13) {
+			System.out.println("초등학생");
+		}else if(age <= 16) {
+			System.out.println("중학생");
+		}else if(age <= 19) {
+			System.out.println("고등학생");
+		}else {
+			System.out.println("성인");
 		}
 		
 	}
