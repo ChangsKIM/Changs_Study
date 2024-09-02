603eb92f8a26e97a51f5e29dd976521fb0e9879a
nam2626 <nam2626@naver.com>
2024-09-02 15:18:22 +0900

    for 문

---
+++ Step04/src/E09_For.java
@@ -0,0 +1,18 @@
+
+public class E09_For {
+	/*
+	 * 		for(초기화; 조건식; 증감식){
+	 * 			실행할 코드...
+	 * 		}
+	 */
+	public static void main(String[] args) {
+		int i = 0;
+		for (i = 0; i < 5; i++) {
+			System.out.println("안녕하세요");
+		}
+	}
+
+}
+
+
+
