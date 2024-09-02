87eb6fbda1e51c7c8b8f4cc202eeeea28e3e08dc
nam2626 <nam2626@naver.com>
2024-09-02 10:10:41 +0900

    문제풀이

--- Step03/src/E14_Quest.java
+++ Step03/src/E14_Quest.java
@@ -27,6 +27,25 @@ public class E14_Quest {
 		else
 			System.out.println("F");
 		
+		//-------------------------------
+		switch(score / 10) {
+		case 10:
+		case 9:
+			System.out.println("A");
+			break;
+		case 8:
+			System.out.println("B");
+			break;
+		case 7:
+			System.out.println("C");
+			break;
+		case 6:
+			System.out.println("D");
+			break;
+		default:
+			System.out.println("F");
+		}
+		
 	}
 
 }
