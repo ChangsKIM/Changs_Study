f79db7c42636936b711697cab1801ad3eb297e52
nam2626 <nam2626@naver.com>
2024-09-03 15:19:08 +0900

    문제 풀이

---
+++ Step05/src/E05_Quest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class E05_Quest {
+	/*
+	 * 	숫자 입력 > 5
+	 * 
+	 *  *
+	 *  **	
+	 *  ***
+	 *  ****
+	 *  *****
+	 * 
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력 : ");
+		int line = sc.nextInt();
+
+		for (int i = 0; i < line; i++) {
+			for(int j = 0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
