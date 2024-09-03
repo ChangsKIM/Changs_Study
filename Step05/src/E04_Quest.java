e708ff00089734dea8059aa7b06ae466bc61b2a8
nam2626 <nam2626@naver.com>
2024-09-03 14:42:13 +0900

    문제 풀이

--- Step05/src/E04_Quest.java
+++ Step05/src/E04_Quest.java
@@ -1,6 +1,20 @@
 
 public class E04_Quest {
-
+	/*
+	 * 	1~10까지의 숫자의 약수 목록을 아래와 같은 형태로 출력
+	 * 	
+	 * 	1
+	 * 	1 2
+	 * 	1 3
+	 * 	1 2 4
+	 *  1 5
+	 *  1 3 6
+	 *  1 7
+	 *  1 2 4 8
+	 *  1 3 9
+	 *  1 2 5 10
+	 * 
+	 */
 	public static void main(String[] args) {
 
 	}
