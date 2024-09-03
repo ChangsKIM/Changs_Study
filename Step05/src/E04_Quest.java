df52d7372ecac4e6e8ae5a347f1acf5b2f3d5130
nam2626 <nam2626@naver.com>
2024-09-03 15:03:24 +0900

    문제 풀이

--- Step05/src/E04_Quest.java
+++ Step05/src/E04_Quest.java
@@ -16,7 +16,19 @@ public class E04_Quest {
 	 * 
 	 */
 	public static void main(String[] args) {
-
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= i; j++) {
+				if(i % j == 0)
+					System.out.print(j + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
+
+
+
+
+
+
