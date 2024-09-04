d2f30d946e9c4c80ed6e3bcd20f200f274eb5ac8
nam2626 <nam2626@naver.com>
2024-09-04 10:53:54 +0900

    continue

--- Step05/src/E08_BookQuest2.java
+++ Step05/src/E08_BookQuest2.java
@@ -8,6 +8,15 @@ public class E08_BookQuest2 {
 			}
 			System.out.println();
 		}
+		//------------------------------------
+		for (int dan = 2; dan < 10; dan++) {
+			if(dan % 2 == 1)
+				continue;
+			for (int i = 1; i < 10; i++) {
+				System.out.printf("%d * %d = %d\n",dan,i,dan*i);
+			}
+			System.out.println();
+		}
 	}
 
 }
