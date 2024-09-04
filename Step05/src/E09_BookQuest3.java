b137a89c96d50850e79f51d6d1b1ef0de2dee5d4
nam2626 <nam2626@naver.com>
2024-09-04 10:36:35 +0900

    break 적용

--- Step05/src/E09_BookQuest3.java
+++ Step05/src/E09_BookQuest3.java
@@ -8,6 +8,16 @@ public class E09_BookQuest3 {
 			}
 			System.out.println();
 		}
+		System.out.println("--------------");
+		//--------------------------------------
+		for (int dan = 2; dan < 10; dan++) {
+			for (int i = 1; i < 10; i++) {
+				if(i > dan)
+					break;
+				System.out.printf("%d * %d = %d\n",dan,i,dan*i);
+			}
+			System.out.println();
+		}
 	}
 
 }
