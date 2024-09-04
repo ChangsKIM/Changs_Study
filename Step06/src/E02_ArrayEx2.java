0f8dd1e3183ea9e2a625d754691f92fb930d8631
nam2626 <nam2626@naver.com>
2024-09-04 12:00:40 +0900

    배열 생성 및 초기화

---
+++ Step06/src/E02_ArrayEx2.java
@@ -0,0 +1,17 @@
+
+public class E02_ArrayEx2 {
+
+	public static void main(String[] args) {
+		//배열 생성과 동시에 초기화 하는 방법 - 1
+		//            0 1 2 3 4 5
+		int[] arr1 = {1,2,3,4,5,6,7,8};
+		
+		System.out.println("배열 개수 확인 : " + arr1.length);
+		
+		for(int i=0;i<arr1.length;i++) {
+			System.out.println(arr1[i]);
+		}
+		//배열 생성과 동시에 초기화 하는 방법 - 2
+	}
+
+}
