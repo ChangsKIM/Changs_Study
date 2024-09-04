83dd14a12eb5955269533b28cd2a95df46baba39
nam2626 <nam2626@naver.com>
2024-09-04 09:52:12 +0900

    문제 풀이

---
+++ Step05/src/E07_BookQuest1.java
@@ -0,0 +1,47 @@
+
+public class E07_BookQuest1 {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 2;
+		char operator = '*';
+		
+		if(operator == '+')
+			System.out.println(num1 + num2);
+		else if(operator == '-')
+			System.out.println(num1 - num2);
+		else if(operator == '*')
+			System.out.println(num1 * num2);
+		else if(operator == '/')
+			System.out.println(num1 / num2);
+		else
+			System.out.println("올바른 연산자가 아닙니다.");
+		
+		//-----------------------------
+		
+		switch(operator) {
+		case '+':
+			System.out.println(num1 + num2);
+			break;
+		case '-':
+			System.out.println(num1 - num2);
+			break;
+		case '*':
+			System.out.println(num1 * num2);
+			break;
+		case '/':
+			System.out.println(num1 / num2);
+			break;
+		default:
+			System.out.println("잘못된 연산자를 입력하셨습니다.");
+		
+		}
+		
+	}
+}
+
+
+
+
+
+
+
