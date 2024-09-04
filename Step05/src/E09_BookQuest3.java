48ac21df13977bb479f4a1dc4d4add6f73fe9047
nam2626 <nam2626@naver.com>
2024-09-04 10:11:33 +0900

    문제풀이

---
+++ Step05/src/E09_BookQuest3.java
@@ -0,0 +1,13 @@
+
+public class E09_BookQuest3 {
+
+	public static void main(String[] args) {
+		for (int dan = 2; dan < 10; dan++) {
+			for (int i = 1; i <= dan; i++) {
+				System.out.printf("%d * %d = %d\n",dan,i,dan*i);
+			}
+			System.out.println();
+		}
+	}
+
+}
