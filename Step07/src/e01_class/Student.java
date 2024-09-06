d521ac97cd9295f250fb8a5cc70ec7170d80aef8
nam2626 <nam2626@naver.com>
2024-09-06 17:13:22 +0900

    클래스, 메서드

--- Step07/src/e01_class/Student.java
+++ Step07/src/e01_class/Student.java
@@ -14,11 +14,27 @@ public class Student {
 	//평점
 	double score;
 	
-	//메서드 
+	/*
+	 * 	메서드(method)
+	 * 		특정 기능에 해당하는 코드를 묶어서 나타내는 것을 메서드라고 함
+	 * 		클래스가 수행할 특정 기능 1개를 표현
+	 * 		메서드를 만들때에는 반드시 하나의 기능만 담당하게끔 만듬
+	 */
+	//리턴 X, 매개변수 X
 	void printStudentInfo() {
 		System.out.println(studentNo + " " + studentName + " " +
 					majorName + " " + score);
 	}
+	//리턴 X, 매개변수 O
+	void init(String no, String name, String major, double s) {
+		//학생데이터 초기화
+		studentNo = no;
+		studentName = name;
+		majorName = major;
+		if(s >= 0 && s <= 4.5)
+			score = s;
+	}
+	
 }
 
 
--- Step07/src/e01_class/StudentMain.java
+++ Step07/src/e01_class/StudentMain.java
@@ -5,10 +5,11 @@ public class StudentMain {
 	public static void main(String[] args) {
 		Student std1 = new Student();//학생 데이터 저장할 인스턴스 생성
 		//데이터 초기화
-		std1.studentNo = "20111234";
-		std1.studentName = "김철수";
-		std1.majorName = "컴퓨터공학과";
-		std1.score = 3.14;
+//		std1.studentNo = "20111234";
+//		std1.studentName = "김철수";
+//		std1.majorName = "컴퓨터공학과";
+//		std1.score = 3.14;
+		std1.init("20111234","김철수","컴퓨터공학과",3.14);
 		
 		std1.printStudentInfo();
 		
