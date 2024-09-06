d566ce7950d32c60c073039737bd9749c30db26e
nam2626 <nam2626@naver.com>
2024-09-06 14:57:47 +0900

    문제 풀이

---
+++ Step06/src/E29_Quest.java
@@ -0,0 +1,33 @@
+
+public class E29_Quest {
+	/*
+	 * 		1   2  3  4  5
+	 * 		10  9  8  7  6
+	 * 		11 12 13 14 15
+	 * 		20 19 18 17 16
+	 * 		21 22 23 24 25
+	 */
+	public static void main(String[] args) {
+		int[][] arr = new int[5][5];
+		
+		int n = 1, sw = 1, col = 0;
+		for (int i = 0; i < arr.length; i++) {
+			//각 행에 숫자 채우기
+			while(col < arr[i].length && col > -1) {
+				arr[i][col] = n++;
+				col += sw;
+			}
+			sw = -sw;
+			col += sw;
+			
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for(int v : arr[i]) {
+				System.out.printf("%2d ",v);
+			}
+			System.out.println();
+		}
+	}
+
+}
