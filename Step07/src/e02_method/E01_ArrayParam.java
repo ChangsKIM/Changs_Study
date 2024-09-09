a86fc41662504b96bb52aadf6f925e8d4955283a
nam2626 <nam2626@naver.com>
2024-09-09 16:51:09 +0900

    배열, 메서드

--- Step07/src/e02_method/E01_ArrayParam.java
+++ Step07/src/e02_method/E01_ArrayParam.java
@@ -10,6 +10,21 @@ public class E01_ArrayParam {
 		System.out.println();
 //		System.out.println(ary);
 	}
+	/*
+	 * 여러개의 값을 받는 방법
+	 * printArgs(1,2,3,4,5,6,7,8,9,10);
+	 * 해당 값들을 ary 배열로 만들어서 처리
+	 * 추가적으로 다른 데이터가 필요하면 
+	 * 가변길이로 선언한 매개변수 앞에 변수 선언을 해야됨
+	 * void printArgs(String str, int ... ary)
+	 */
+	static void printArgs(int ... ary) {
+		for (int i = 0; i < ary.length; i++) {
+			System.out.print(ary[i]+" ");
+		}
+		System.out.println();
+	}
+	
 	//배열의 내용을 1씩 증가시키는 메서드
 	static void addArray(int[] ary) {
 		for(int i=0;i<ary.length;i++) {
@@ -26,7 +41,8 @@ public class E01_ArrayParam {
 		printArray(arr);
 		printArray(new int[] {1,2,3,4,5,6,7,8,9});
 		printArray(new int[] {1,2,3,4,5,6,7,8,9});
-		
+		printArgs(1,2,3,4,5,6,7,8,9,10);
+		printArgs(1,2,3,8,9,10);
 	}
 
 }
