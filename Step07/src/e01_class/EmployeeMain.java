41811caf69f51a647d23c40e919bac5f834079eb
nam2626 <nam2626@naver.com>
2024-09-09 10:54:25 +0900

    클래스, 메서드

---
+++ Step07/src/e01_class/EmployeeMain.java
@@ -0,0 +1,12 @@
+package e01_class;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		//new -> 메모리 할당, 생성된 Employee 객체(instance)
+		Employee emp = new Employee();
+		emp.init("20101111", "김철수", false, "부장", "총무부", 7800);
+		emp.printEmployeeInfo();
+	}
+
+}
