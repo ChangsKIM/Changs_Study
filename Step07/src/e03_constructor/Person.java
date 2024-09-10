8e248be4c96b8f1349aeaba183d46987bb52145f
nam2626 <nam2626@naver.com>
2024-09-10 14:11:35 +0900

    생성자

---
+++ Step07/src/e03_constructor/Person.java
@@ -0,0 +1,26 @@
+package e03_constructor;
+//이름, 나이 저장하는 클래스
+//이름 나이 출력하는 기능
+//출력 형태 : 이름 / 나이
+public class Person {
+	String name;
+	int age;
+	
+	/*
+	 *	생성자(Constructor)
+	 *		생성자는 객체를 하나 생성 할때 제일 먼저 실행되는 메서드
+	 *		만약에 생성자를 직접 작성하지 않으면,
+	 *		자바 컴파일러가 자동으로 아무런 일도 안하는 기본 생성자를 추가 
+	 *		Person() { }
+	 *		1. 리턴타입 없음
+	 *		2. 메서드 이름이 클래스 이름과 같다.
+	 * 
+	 */
+	Person(){
+		System.out.println("Person 기본 생성자");
+	}
+	
+	void printPersonInfo() {
+		System.out.println(name + " / " + age);
+	}
+}
