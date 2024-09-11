f1cf8c5a5bbe0195ed3cdd0f4149a9c66eff6a88
nam2626 <nam2626@naver.com>
2024-09-11 14:02:01 +0900

    클래스 작성

---
+++ Step07/src/e04_access/Square.java
@@ -0,0 +1,44 @@
+package e04_access;
+
+public class Square {
+	private int width;
+	private int height;
+
+	public Square(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+	//Alt + Shift + s -> r
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int squareArea() {
+		return width * height;
+	}
+	
+	@Override
+	public String toString() {
+		return width + " / " + height + " / " + squareArea();
+	}
+	
+	
+	
+	
+	
+}
+
+
+
