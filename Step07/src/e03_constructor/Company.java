7138c5768842051be65f878f146a59fa0369ccf1
nam2626 <nam2626@naver.com>
2024-09-11 10:55:40 +0900

    toString

--- Step07/src/e03_constructor/Company.java
+++ Step07/src/e03_constructor/Company.java
@@ -16,6 +16,13 @@ public class Company {
 	public Company(String companyName, String compantTel) {
 		this(companyName,compantTel,100,"https://test.com");
 	}
+
+	//Alt + Shift + s -> s
+	@Override
+	public String toString() {
+		return companyName + " / " + compantTel + " / " + employeeCount
+				+ " / " + compantURL;
+	}
 	
 	
 	
---
+++ Step07/src/e03_constructor/CompanyMain.java
@@ -0,0 +1,15 @@
+package e03_constructor;
+
+public class CompanyMain {
+
+	public static void main(String[] args) {
+		Company c1 = 
+			new Company("철강소", "02-3333-4444", 300, "https://aaaa.gg");
+		
+		Company c2 = new Company("목공소", "1111-4444");
+		
+		System.out.println(c1);
+		System.out.println(c2);
+	}
+
+}
