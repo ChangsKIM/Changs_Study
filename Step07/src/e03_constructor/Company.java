1d16db4112d5d4cfe71e1fde4f268dce4645a868
nam2626 <nam2626@naver.com>
2024-09-11 10:45:58 +0900

    this

---
+++ Step07/src/e03_constructor/Company.java
@@ -0,0 +1,23 @@
+package e03_constructor;
+
+public class Company {
+	String companyName;
+	String compantTel;
+	int employeeCount;
+	String compantURL;
+	
+	public Company(String companyName, String compantTel, int employeeCount, String compantURL) {
+		this.companyName = companyName;
+		this.compantTel = compantTel;
+		this.employeeCount = employeeCount;
+		this.compantURL = compantURL;
+	}
+
+	public Company(String companyName, String compantTel) {
+		this(companyName,compantTel,100,"https://test.com");
+	}
+	
+	
+	
+	
+}
