1eee3722c5c8e8194761dbf9fa55821b63abd475
nam2626 <nam2626@naver.com>
2024-09-12 10:12:50 +0900

    Student Project

---
+++ StudentProject_ ver1/src/service/StudentService.java
@@ -0,0 +1,29 @@
+package service;
+
+import dto.Student;
+
+/*
+ * 	학생 데이터를 배열로 관리
+ * 	작업 배열에 학생 데이터 추가, 삭제, 검색, 수정
+ * 
+ */
+public class StudentService {
+	//학생정보 저장할 배열
+	private Student[] arr;
+	//배열을 관리할 인덱스
+	private int idx;
+	
+	public StudentService() {
+		arr = new Student[5];
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
