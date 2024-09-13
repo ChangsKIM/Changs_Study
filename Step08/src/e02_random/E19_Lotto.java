ee58c0fce82ef1a3a369459baeab83598a78e14f
nam2626 <nam2626@naver.com>
2024-09-13 11:04:02 +0900

    로또번호 한셋트 생성

--- Step08/src/e02_random/E19_Lotto.java
+++ Step08/src/e02_random/E19_Lotto.java
@@ -2,6 +2,22 @@ package e02_random;
 import java.util.Random;
 
 public class E19_Lotto {
+	//로또번호 한셋트 리턴하는 메서드
+	public static int[] generateLotto() {
+		int[] arr = new int[6];
+		
+		//랜덤으로 배열에 숫자를 저장, 저장할 범위 : 1~10
+		for(int i=0;i<arr.length;i++) {
+			int n = generateLottoNumber();
+			if(isDuplicateNumber(arr, n)) {
+				i--;
+				continue;
+			}
+			arr[i] = n;
+		}
+		return arr;
+	}
+	
 	//1~45 숫자 리턴하는 메서드
 	public static int generateLottoNumber() {
 		Random r = new Random();
@@ -18,34 +34,16 @@ public class E19_Lotto {
 	
 	//중복 체크하는 메서드
 	public static boolean isDuplicateNumber(int[] arr, int n) {
-		
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i] == n)
 				return true;
 		}
-		
 		return false;
 	}
 	
 	public static void main(String[] args) {
-		//랜덤 클래스 생성
-		Random r = new Random();
-		
 		for(int c=0;c<5;c++) {
-			//로또번호 1셋트 랜덤으로 생성해서 출력, 1 ~ 45
-			int[] arr = new int[6];
-
-			//랜덤으로 배열에 숫자를 저장, 저장할 범위 : 1~10
-			for(int i=0;i<arr.length;i++) {
-				int n = generateLottoNumber();
-				if(isDuplicateNumber(arr, n)) {
-					i--;
-					continue;
-				}
-				arr[i] = n;
-			}
-			//출력
-			printLottoNumber(arr);
+			printLottoNumber(generateLotto());
 		}
 		
 	}
