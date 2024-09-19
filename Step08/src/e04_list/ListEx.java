41fbc594ff9a748b7d1b013b898395a291b45c1a
nam2626 <nam2626@naver.com>
2024-09-19 12:01:01 +0900

    리스트 예제

---
+++ Step08/src/e04_list/ListEx.java
@@ -0,0 +1,38 @@
+package e04_list;
+
+import java.util.ArrayList;
+
+public class ListEx {
+
+	public static void main(String[] args) {
+		//리스트 생성
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		//데이터 추가
+		list.add(20);
+		list.add(1);
+		list.add(55);
+		list.add(65);
+		list.add(34);
+		list.add(27);
+		System.out.println(list);
+		
+		//저장된 데이터 개수
+		System.out.println("리스트에 저장된 값 개수 : " + list.size());
+		//데이터 전체 조회
+//		System.out.println(list.get(0));
+		for(int i = 0;i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		//데이터 삭제
+		list.
+	}
+
+}
+
+
+
+
+
+
+
