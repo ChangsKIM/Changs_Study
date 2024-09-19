807e8f3ea9149bccac8cf645c29c5c4979b25e76
nam2626 <nam2626@naver.com>
2024-09-19 17:09:10 +0900

    학생 정보 수정

--- StudentProject_ver2/src/service/StudentService.java
+++ StudentProject_ver2/src/service/StudentService.java
@@ -102,7 +102,30 @@ public class StudentService {
 	}
 
 	public void updateStudent() {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String studentNo;
+		//학번 입력 받음
+		System.out.println("학생 정보 수정을 시작합니다.....");
+		System.out.print("수정할 학번 입력 : ");
+		studentNo = sc.nextLine(); 
+		
+		Student std = searchStudent(studentNo);
+		
+		if(std == null) {
+			System.out.println("수정할 학생 정보가 없습니다.");
+			return;
+		}
+		System.out.print("수정할 이름 입력 : ");
+		String studentName = sc.nextLine();
+		std.setStudentName(studentName);
+		System.out.print("수정할 학과명 입력 : ");
+		String majorName = sc.nextLine();
+		std.setMajorName(majorName);
+		System.out.print("수정할 학생 평점 입력 : ");
+		double score = sc.nextDouble();
+		std.setScore(score);
+		
+		System.out.println("학생정보 수정이 완료되었습니다.");
 		
 	}
 
