c22d67bd945e45717cedce49b00438354a51153b
nam2626 <nam2626@naver.com>
2024-09-20 16:15:14 +0900

    파트타임 직원

---
+++ Step09/src/e04_example/EmployeeMain.java
@@ -0,0 +1,10 @@
+package e04_example;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		PermanentWorker pwoker = new PermanentWorker(3600, "김철수");
+		pwoker.printEmployeeInfo();
+	}
+
+}
---
+++ Step09/src/e04_example/PartTimeWorker.java
@@ -0,0 +1,15 @@
+package e04_example;
+
+public class PartTimeWorker extends Employee{
+	//일한 시간, 시급
+	private int workTime;
+	private int payPerHour;
+
+	public PartTimeWorker(String name, int workTime, int payPerHour) {
+		super(0, name);
+		this.workTime = workTime;
+		this.payPerHour = payPerHour;
+	}
+	
+	
+}
