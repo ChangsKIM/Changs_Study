82eb932fe3298e60d1519cf696e30b9d8a4f03c2
nam2626 <nam2626@naver.com>
2024-09-20 10:53:49 +0900

    상속 기본 예제

--- Step07/src/e02_method/E05_RecursiveMethod.java
+++ Step07/src/e02_method/E05_RecursiveMethod.java
@@ -9,10 +9,16 @@ public class E05_RecursiveMethod {
 			return 1;
 		return n * fac(n-1);		
 	}
-	
+	//매개변수로 숫자 하나를 받아서 숫자에 해당하는 2의 n승 값을 리턴하는 메서드
+	//매개변수로 값이 6 ---> 리턴할 값 64  2의 6승값 리턴
+	public static int power(int n) {
+		if(n == 1)
+			return 2;
+		return 2 * power(n-1);	
+	}
 	public static void main(String[] args) {
 		System.out.println(fac(5));
-
+		System.out.println(power(10));
 	}
 
 }
---
+++ Step09/src/e01_inheritance/AnimalMain.java
@@ -0,0 +1,10 @@
+package e01_inheritance;
+
+public class AnimalMain {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.eat();
+	}
+
+}
