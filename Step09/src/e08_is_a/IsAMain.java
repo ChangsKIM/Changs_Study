349b9227a29122c947108eb66a3d97a89fd2347a
nam2626 <nam2626@naver.com>
2024-09-23 15:13:28 +0900

    is a

--- Step09/src/e08_is_a/IsAMain.java
+++ Step09/src/e08_is_a/IsAMain.java
@@ -3,7 +3,36 @@ package e08_is_a;
 public class IsAMain {
 
 	public static void main(String[] args) {
+		Person p = new Person();
+		Dog d = new Dog();
 		
+		Animal a = p;
+		a.eat();
+		Person e = (Person)a;
+		e.eat();
+		System.out.println(System.identityHashCode(p));
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(e));
+		
+		a = d;
+		a.eat();
+		d.eat();
+		Dog g = (Dog) a;
+		g.eat();
+		System.out.println(System.identityHashCode(a));
+		System.out.println(System.identityHashCode(d));
+		System.out.println(System.identityHashCode(g));
+		
+		Animal al = d;
+		Person pr = (Person) al;
+		pr.eat();
 	}
 
 }
+
+
+
+
+
+
+
