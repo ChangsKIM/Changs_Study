3b022bfceb21b6ed37b91fa53c95e7381620a440
nam2626 <nam2626@naver.com>
2024-09-23 11:55:16 +0900

    상속 예제

--- Step09/src/e05_quest/IotApp.java
+++ Step09/src/e05_quest/IotApp.java
@@ -1,6 +1,7 @@
 package e05_quest;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class IotApp {
 	private ArrayList<Power> list;
@@ -14,7 +15,25 @@ public class IotApp {
 	}
 	
 	//전체 전원 On
+	public void powerAllOnDevice() {
+		for(Power p : list) {
+			p.powerOn();
+		}
+		
+//		for(int i=0;i<list.size();i++) {
+//			list.get(i).powerOn();
+//		}
+	}
 	
 	//전체 전원 Off
+	public void powerAllOffDevice() {
+		for(Power p : list) {
+			p.powerOff();
+		}
+	}
 	
 }
+
+
+
+
