4200e920999dad7ac00d77127ee35e2c16a23331
nam2626 <nam2626@naver.com>
2024-09-23 10:40:48 +0900

    상속 예제

---
+++ Step09/src/e05_quest/Power.java
@@ -0,0 +1,31 @@
+package e05_quest;
+/*
+ * 전원 관리하는 클래스
+ * 		전원 값 
+ * 		전원을 켜고끄고하는 메서드
+ * 		전원을 켜는 메서드, 전원을 끄는 메서드
+ */
+public class Power {
+	private boolean power;
+
+	public void powerOn() {
+		power = true;
+	}
+	
+	public void powerOff() {
+		power = false;
+	}
+	
+	public void powerOnOff() {
+		power = !power;
+	}
+}
+
+
+
+
+
+
+
+
+
