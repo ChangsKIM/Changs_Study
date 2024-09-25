2ac138cae53ce8eb961523d856424f5a4036d077
nam2626 <nam2626@naver.com>
2024-09-25 13:39:52 +0900

    맵

--- Step10/src/e02_collection/MapTest.java
+++ Step10/src/e02_collection/MapTest.java
@@ -38,17 +38,21 @@ public class MapTest {
 		//키값에 해당하는 데이터가 없으면 null이 나옴
 		System.out.println(map.get("C"));
 		
-		//데이터 삭제
-		
+		//데이터 삭제 - 키값으로 삭제
+		map.remove("B");
+		System.out.println(map);
 		//맵에 저장된 데이터 개수
-		
+		System.out.println(map.size());
 		//맵에 해당 키값이 있는지?
-		
+		System.out.println(map.containsKey("A"));
+		System.out.println(map.containsKey("B"));
 		//맵에 해당 데이터가 있는지?
-		
+		System.out.println(map.containsValue(new Point(10,4)));
 		//맵에 내용 비우기
-		
+		map.clear();
+		System.out.println(map);
 		//맵에 내용이 비었는지?
+		System.out.println(map.isEmpty());
 		
 	}
 
