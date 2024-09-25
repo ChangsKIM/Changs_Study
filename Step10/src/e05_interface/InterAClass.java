80b819675cb07e643720bc0b580175c2ae5ac9f6
nam2626 <nam2626@naver.com>
2024-09-25 15:19:32 +0900

    인터페이스

--- Step10/src/e05_interface/InterAClass.java
+++ Step10/src/e05_interface/InterAClass.java
@@ -1,5 +1,10 @@
 package e05_interface;
 
-public class InterAClass {
+public class InterAClass implements InterA{
+
+	@Override
+	public void interA() {
+		System.out.println("InterA Class A");
+	}
 
 }
---
+++ Step10/src/e05_interface/InterBClass.java
@@ -0,0 +1,10 @@
+package e05_interface;
+
+public class InterBClass implements InterA{
+
+	@Override
+	public void interA() {
+		System.out.println("InterA Class B");
+	}
+
+}
