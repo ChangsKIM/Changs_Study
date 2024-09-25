cf969de69679dc1427416603b3100cfdc350abdd
nam2626 <nam2626@naver.com>
2024-09-25 14:34:35 +0900

    추상 클래스

--- Step10/src/e03_abstract/Animal.java
+++ Step10/src/e03_abstract/Animal.java
@@ -2,6 +2,9 @@ package e03_abstract;
 /*
  * 		추상 클래스(Abstract Class)
  * 			- class 키워드 앞에 abstract가 붙음
+ * 			- 추상 클래스는 직접적으로 생성되지 않음
+ * 			- 자식 클래스가 생성이 될 때만 간접적으로 생성
+ * 			- 일반적으로는 최상위 클래스로 존재(최초에 클래스의 구조를 잡는 용도)
  * 
  */
 public abstract class Animal {
