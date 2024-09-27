8537810758449db20b91d744091aa575113f1edb
nam2626 <nam2626@naver.com>
2024-09-27 16:02:29 +0900

    exception

--- Step11/src/e03_exception/ExceptionEx6.java
+++ Step11/src/e03_exception/ExceptionEx6.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class ExceptionEx6 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
+		try {
 			Scanner sc = new Scanner(System.in);
 
 			System.out.print("숫자 입력 : ");
@@ -15,6 +16,9 @@ public class ExceptionEx6 {
 			if(n2 == 0)
 				throw new Exception("0으로 나누면 안됩니다.");
 			System.out.println(n1 / n2);
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
