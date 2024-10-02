86a409ec3944bd0ce62bf1d0b332ac39c458687a
nam2626 <nam2626@naver.com>
2024-10-02 14:12:04 +0900

    스레드 우선순위, 서버 프로그램 기초

--- Thread/src/e02_priority/ThreadPriority.java
+++ Thread/src/e02_priority/ThreadPriority.java
@@ -11,7 +11,7 @@ public class ThreadPriority extends Thread{
 	public void run() {
 		for(int i=0;i<10;i++) {
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
