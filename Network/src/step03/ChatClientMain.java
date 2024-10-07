e25f3e177f54b1c00fdd352742b3c35e6587ca09
nam2626 <nam2626@naver.com>
2024-10-07 14:49:37 +0900

    주석 추가

--- Network/src/step03/ChatClientMain.java
+++ Network/src/step03/ChatClientMain.java
@@ -36,10 +36,10 @@ public class ChatClientMain {
 		try(Socket server = new Socket("127.0.0.1",3333);
 				Scanner sc = new Scanner(System.in)){
 			System.out.println("서버에 접속했습니다.");
-			
+			//상대방이 보낸 메세지를 받아서 출력하는 스레드 실행
 			Worker worker = new Worker(server);
 			worker.start();
-			
+			//사용자가 메세지 입력 후 서버로 전송하는 부분
 			PrintWriter pw = new PrintWriter(server.getOutputStream());
 			while(true) {
 				System.out.println("보낼 메세지 작성");
