299751851adde14ed7ccd8b52e5e606a51126ce3
nam2626 <nam2626@naver.com>
2024-10-07 14:19:06 +0900

    채팅 클라이언트

---
+++ Network/src/step03/ChatClientMain.java
@@ -0,0 +1,33 @@
+package step03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClientMain {
+
+	public static void main(String[] args) {
+		try(Socket server = new Socket("127.0.0.1",3333);
+				Scanner sc = new Scanner(System.in)){
+			System.out.println("서버에 접속했습니다.");
+			
+			PrintWriter pw = new PrintWriter(server.getOutputStream());
+			while(true) {
+				System.out.println("보낼 메세지 작성");
+				String str = sc.nextLine();
+				pw.println(server.getInetAddress() 
+						+" 님이 보낸 메세지 : " + str);
+				pw.flush();
+				if(str.equals("exit")) break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
